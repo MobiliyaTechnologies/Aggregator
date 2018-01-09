981bed74a229b667cc6732169388ac0baa2e6c47
komal.pharate <komal.pharate@mobiliya.com>
2018-01-09 12:13:59 +0530

    removing Aggregator folder and added ReadMe.md

--- Aggregator/stopAllCamera.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("ps -ef |grep livestreaming.py | awk '{print $2}'| xargs kill -9")
-print ("Stopped live streaming")
-'''
-os.system("kill -f livestream")
-os.system("kill -f detectnet")
-'''

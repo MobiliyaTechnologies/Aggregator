2d96afa9b235de6ab3aa5f12f146df0ab4b5b32f
Komal Pharate <komal.pharate@mobiliya.com>
2018-02-12 18:43:46 +0530

    raw image Directory added

--- config.js
+++ config.js
@@ -5,8 +5,10 @@ var config = {};
 
 config.port = 3010;
 config.imageDirectory='./Images360';
+config.rawImageDirectory = './RawImages';
+
 config.imageTargetDirectory = './Images360Target';
-config.aggregatorName = "Aggregator 10";
+config.aggregatorName = "Aggregator06";
 config.location = "4rth Floor Amar Apex";
 config.channelId = "32";
 config.url = "rtsp://<username>:<password>@<ip_address>:<port>/cam/realmonitor?channel=<id>&subtype=0";
@@ -19,9 +21,14 @@ config.availability = "yes";
 config.mqttBroker = "mqtt://10.9.44.101:1889";
 
 //config.host = "http://52.177.169.81:5008";
-config.host = "https://snsbackend.mobiliya.com:5009";
+// config.host = "https://snsbackend.mobiliya.com:5009";10.9.42.211:5009
+config.host = "http://10.9.42.211:5009";
 
-config.cloudComputeEngine = "http://52.177.169.81:5003";
+
+config.cloudComputeEngine = "http://10.9.44.101:5003";
+// config.cloudComputeEngine = "http://10.9.43.130:5004";
+
+config.cloudFaceRecognitionComputeEngine = "http://10.9.43.130:5004";
 
 config.registerAggregator = config.host + "/devices/aggregators";
 /**
@@ -29,8 +36,8 @@ config.registerAggregator = config.host + "/devices/aggregators";
  */
 // config.jetsonFolderPath = "ubuntu@10.9.43.63:/home/ubuntu/surveillance/jetson-dl/jetson-inference/build/aarch64/bin/Cameras/Cam";
 // config.jetsonFolderPath = "ubuntu@10.9.43.63:/home/ubuntu/Compute-Engine-Yolo/darknet/Cameras/Cam";
-config.jetsonFolderPath = "nvidia@10.9.43.74:/home/nvidia/Compute-Engine-Yolo/darknet/Cameras/Cam";
-/**
+config.jetsonFolderPath = "nvidia@10.9.43.75:/home/nvidia/Compute-Engine-Yolo/darknet/Cameras/Cam";
+/** 
  * URL of VM to send images
  */
 config.sendLiveStreamUploadURL = config.host + "/images";
@@ -38,7 +45,9 @@ config.sendLiveStreamUploadURL = config.host + "/images";
 config.camFolder = "./Cameras";
 config.livestreamingCamFolder = "./Cameras/Cam";
 
-config.cloudServiceUrl = config.cloudComputeEngine + "/faces";
+config.cloudServiceFaceDetectionUrl = config.cloudComputeEngine + "/faces";
+config.cloudServiceFaceRecognizeUrl = config.cloudComputeEngine + "/faces/recognize";
+
 config.cloudServiceTargetUrl = config.host + "/results";
 
 module.exports = config;

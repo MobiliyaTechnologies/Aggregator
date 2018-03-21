a0331825cb799884304fff5447fe82b4e6319434
Komal Pharate <komal.pharate@mobiliya.com>
2018-03-21 15:22:27 +0530

    image transfer functions added to image trannsfer file

--- controllers/liveStreamingController.js
+++ controllers/liveStreamingController.js
@@ -1,16 +1,16 @@
 var config = require('../config');
 var base64_encode = require('./imageProcessingController').base64_encode;
+var imageTransfer = require('../controllers/imageTransfer');
 
 var parseJson = require('parse-json');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
-var Rsync = require('rsync');
 const cv = require('opencv4nodejs');
-var request = require('request');
 
 //to keep track of live cameras
 var liveCamIntervalArray = [];
 var sendImagesToggleMap = new Map();
+
 
 /**
 * to create Cam<CamId> folder and call startStreaming
@@ -161,7 +161,7 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
                 /**reading next frame */
                 if (vCap != null) {
                     let frame = vCap.read();
-                    if (countframe % fps == 0) {
+                    if (countframe %fps== 0) {
                         //countframe reset
                         countframe = 0;
                         var timestamp = new Date().getTime();
@@ -177,7 +177,7 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
                             //Send images to Backend
                         if(sendImagesToggleMap.get(camId) || parsedJson.sendImagesFlag)
                         {
-                            sendImages(imageName, imageFullPath, function () {
+                            imageTransfer.sendImages(imageName, imageFullPath, function () {
                                 sentImage = 1;
                             });
                         }
@@ -190,7 +190,7 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
                                 /**
                                 * to sync newly added file with compute engine's FS
                                 */
-                                rsyncInterval(0, imageName, imageFullPath, camId, jetsonFolderPath);
+                               imageTransfer.rsyncInterval(0, imageName, imageFullPath, camId, jetsonFolderPath);
                                 //deleting the sent file 
                                 //console.log("IMG path :: ", imageFullPath);
                                 break;
@@ -199,14 +199,14 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
                                 /**
                                 * to send images to cloud compute engine
                                 */
-                                sendImageCloudComputeEngine(timestamp, imageFullPath, bboxes, imageConfig, config.cloudServiceTargetUrl, cloudServiceUrl,camName); // cloudServiceUrl
+                               imageTransfer.sendImageCloudComputeEngine(timestamp, imageFullPath, bboxes, imageConfig, config.cloudServiceTargetUrl, cloudServiceUrl,camName); // cloudServiceUrl
                                 break;
 
                             case 'faceRecognition':
                                 /**
                                 * to send images to cloud compute engine
                                 */
-                                sendImageCloudComputeEngine(timestamp, imageFullPath, bboxes, imageConfig, config.cloudServiceTargetUrl, cloudServiceUrl,camName);
+                               imageTransfer.sendImageCloudComputeEngine(timestamp, imageFullPath, bboxes, imageConfig, config.cloudServiceTargetUrl, cloudServiceUrl,camName);
                                 break;
 
                             default:
@@ -228,129 +228,6 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
         }
     });
 
-}
-
-/**
- * to send images to Cloud Compute Engine using API post
- * @param {*} timestamp image timestamp
- * @param {*} imageFullPath 
- * @param {*} bboxes AOI for detection
- * @param {*} imageConfig configuration for managing co-ordinates
- * @param {*} cloudServiceTargetUrl url of cloud api to be used by cloud compute engine
- * @param {*} cloudServiceUrl url of cloud compute engine
- */
-var sendImageCloudComputeEngine = function (timestamp, imageFullPath, bboxes, imageConfig, cloudServiceTargetUrl, cloudServiceUrl,camName) {
-    //console.log("**SENDImageTOCloud");
-
-    //console.log("Cloud Service URL ::", cloudServiceUrl);
-    //console.log("IMG config : ", imageConfig);
-
-    //connect with cloudServiceUrl
-    var requestObj = request.post(cloudServiceUrl, function optionalCallback(err, httpResponse, body) {
-        if (err) {
-            return console.error('Failed to connect to compute engine:', err);
-        }
-        console.log('Compute engine respond : ', parseJson(body).result);
-    });
-
-    //send the image
-    var form = requestObj.form();
-    form.append('areaOfInterest', JSON.stringify(bboxes));
-    form.append('deviceName', camName);
-    form.append('targetUrl', cloudServiceTargetUrl);
-    form.append('timestamp', timestamp);
-    form.append('imageConfig', JSON.stringify(imageConfig));
-
-    form.append('file',
-        fs.createReadStream(imageFullPath).on('end', function () {
-            console.log("***File sent to compute engine***");
-        })
-    );
-};
-
-/**
- * rsync images to compute engine's FS
- * @param {*} timeInterval 
- * @param {*} imgName 
- * @param {*} imgPath 
- * @param {*} camId 
- * @param {*} jetsonFolderPath 
- */
-var rsyncInterval = function (timeInterval, imgName, imgPath, camId, jetsonFolderPath) {
-
-    //console.log("CAMERA ID  ::", camId);
-    //console.log("RSYNC TARGET ::", jetsonFolderPath);
-
-    //CMD
-    var rsync = new Rsync()
-        .shell('ssh')
-        .flags('avz')
-        .source(imgPath)
-        .destination(jetsonFolderPath + camId);
-
-    //if DWARPED image send it after specified interval
-    if (timeInterval !== 0) {
-        setTimeout(function () {
-            //console.log("\n\nRSYNC PATH______________________", imgPath);
-            rsync.execute(function (error, code, cmd) {
-                if (error)
-                    console.log("Error in rsync ::", error);
-                else {
-                    clearInterval(rsyncInterval);
-                    console.log("--Rsync done of ", imgName);
-                }
-            });
-        }, timeInterval);
-    }
-    //if normal image send it directly
-    else {
-        rsync.execute(function (error, code, cmd) {
-            //console.log("In non-mobile camera!!",cmd);
-            if (error)
-                console.log("Error in rsync ::", error);
-            else {
-                console.log("--Rsync done of ", imgName);
-                clearInterval(rsyncInterval);
-            }
-        });
-    }
-}
-
-/**
- * sending images via API
- * @param {*} imgName 
- * @param {*} imgPath 
- */
-var sendImages = function (imgName, imgPath, callback) {
-
-    //console.log("SEND IMAGES :: Img name : " + imgName + " Img Path :" + imgPath);
-
-    //convert to base64
-    var base64Img = base64_encode(imgPath);
-    base64Img = "data:image/jpg;base64, " + base64Img;
-
-    //console.log("base64 img ::: ", base64Img);
-    var imgJsonBody = {
-        imgName: imgName,
-        imgBase64: base64Img
-    };
-
-    var options = {
-        rejectUnauthorized: false,
-        url: config.sendLiveStreamUploadURL,
-        method: 'POST',
-        json: imgJsonBody
-    }
-    request(options, function (error, body, response) {
-        if (error) {
-            console.log("ERROR in posting image::" + error);
-        }
-        else {
-            //fs.unlinkSync(imgPath);
-            console.log("++BACKEND: Response for image:: " + imgJsonBody.imgName + " => " + JSON.stringify(body.statusCode));
-            callback();
-        }
-    });
 }
 
 /** 
@@ -379,6 +256,11 @@ var stopCamera = function (message, callback) {
     callback(null);
 };
 
+/**
+ * images toggling
+ * @param {*} camId 
+ * @param {*} flag 
+ */
 var toggleSendImageFlag = function(camId, flag){
     sendImagesToggleMap.set(camId,flag);
     console.log("Flag Toggled to "+flag+" for camera id :",camId);
@@ -386,9 +268,6 @@ var toggleSendImageFlag = function(camId, flag){
 
 module.exports.createCameraFolder = createCameraFolder;
 module.exports.startLiveStreaming = startLiveStreaming;
-module.exports.sendImageCloudComputeEngine = sendImageCloudComputeEngine;
-module.exports.sendImages = sendImages;
-module.exports.rsyncInterval = rsyncInterval;
 module.exports.stopCamera = stopCamera;
 module.exports.openStream = openStream;
 module.exports.toggleSendImageFlag = toggleSendImageFlag;

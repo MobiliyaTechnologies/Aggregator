ce7938aafde8760acebea1d4d9214dc93ff63436
Komal Pharate <komal.pharate@mobiliya.com>
2018-04-13 10:38:03 +0530

    added client.open() in iotHub

--- mqtt/mqttCommunication.js
+++ mqtt/mqttCommunication.js
@@ -24,102 +24,109 @@ function printResultFor(op) {
 var topicSubscribe = function (deviceConnectionString) {
     console.log("In iot hub subcribe", deviceConnectionString);
     client = clientFromConnectionString(deviceConnectionString);
+    client.open(function (error) {
+        if (error)
+            console.log("Error in connecting:", error);
+        else {
+            console.log("Connected");
 
-    client.on('message', function (message) {
-        console.log('Id: ' + message.messageId + ' Body: ' + message.data);
-        client.complete(message, printResultFor('completed'));
-        var topic = message.messageId;
-        var message = message.data;
-        // message is Buffer
-        console.log("DATA RECEIVED ON TOPIC :: ", topic);
+            client.on('message', function (message) {
+                console.log('Id: ' + message.messageId + ' Body: ' + message.data);
+                client.complete(message, printResultFor('completed'));
+                var topic = message.messageId;
+                var message = message.data;
+                // message is Buffer
+                console.log("DATA RECEIVED ON TOPIC :: ", topic);
 
-        // logger.debug("Data received on topic : ",topic);
-        switch (topic) {
-            case '/':
-                {
-                    console.log("MQTT==================Project Heimdall Aggregator Server Available to Respond!!\n-----------------------------------\n");
-                    break;
+                // logger.debug("Data received on topic : ",topic);
+                switch (topic) {
+                    case '/':
+                        {
+                            console.log("MQTT==================Project Heimdall Aggregator Server Available to Respond!!\n-----------------------------------\n");
+                            break;
+                        }
+                    /**
+                     * Camera Adding
+                     */
+                    case "checkCamera":
+                        var newDevice = message.toString();
+                        checkCamera(newDevice, function (error) {
+                            console.log("MQTT==================checkCamera Done!!\n-----------------------------------\n");
+                        });
+                        break;
+                    /**
+                     * Raw Image
+                     */
+                    case "getRawImage":
+                        var sendData = message.toString();
+                        var parsedJson = parseJson(sendData);
+
+                        getRawImage(message, function (error) {
+                            if (!error) {
+                                console.log("MQTT==================getRawImage Topic Serving Done!!\n-----------------------------------\n");
+                            }
+                            else
+                                console.log("**Error in GetRawImage :", error);
+                        });
+                        break;
+
+                    /**
+                     * Streaming 
+                     */
+                    case "startStreaming":
+                        var sendData = message.toString();
+                        var parsedJson = parseJson(sendData);
+                        //console.log("Data to stream ::", parsedJson);
+
+                        liveStreamController.createCameraFolder(sendData, function (parsedJson, cameraFolder) {
+                            if (parsedJson.deviceType !== "Mobile") {
+                                liveStreamController.startLiveStreaming(parsedJson, cameraFolder);
+                                console.log("MQTT==================Start Streaming!!\n-----------------------------------\n");
+                            }
+                        });
+                        break;
+
+                    /**
+                     * Stop streaming
+                     */
+                    case "stopCamera":
+                        var camIds = message.toString();
+                        console.log("\n*Stop these cameras ::", JSON.stringify(camIds));
+                        liveStreamController.stopCamera(camIds, function (error) {
+                            if (!error) {
+                                console.log("MQTT==================Stopped the Streaming Cameras\n-----------------------------------\n");
+                            }
+                        });
+
+                        break;
+                    /**
+                     * Stop/Start sending images to backend for a specific camera
+                     */
+                    case "toggleSendImageFlag":
+                        var toggleObj = JSON.parse(message.toString());
+                        console.log("Incoming : ", toggleObj.flag);
+                        console.log("Incoming : ", toggleObj);
+                        if (toggleObj.flag === 0 || toggleObj.flag === 1) {
+                            liveStreamController.toggleSendImageFlag(toggleObj.camId, toggleObj.flag);
+                        } else {
+                            console.log("Error in ToggleSendImageFlag :: Invalid flag");
+                        }
+                        break;
+
+                    /**
+                     * video indexing
+                     */
+                    case "videoIndexingTopic":
+                        var videoSourceData = message.toString();
+                        var parsedJson = parseJson(videoSourceData);
+                        videoIndexing(parsedJson);
+                        console.log("Data sent for video indexing");
+                        break;
+
+                    default:
+                        console.log("\n Default ::  Topic:: " + topic + " not handled!!");
                 }
-            /**
-             * Camera Adding
-             */
-            case "checkCamera":
-                var newDevice = message.toString();
-                checkCamera(newDevice, function (error) {
-                    console.log("MQTT==================checkCamera Done!!\n-----------------------------------\n");
-                });
-                break;
-            /**
-             * Raw Image
-             */
-            case "getRawImage":
-                var sendData = message.toString();
-                var parsedJson = parseJson(sendData);
-
-                getRawImage(message, function (error) {
-                    if (!error) {
-                        console.log("MQTT==================getRawImage Topic Serving Done!!\n-----------------------------------\n");
-                    }
-                    else
-                        console.log("**Error in GetRawImage :", error);
-                });
-                break;
-
-            /**
-             * Streaming 
-             */
-            case "startStreaming":
-                var sendData = message.toString();
-                var parsedJson = parseJson(sendData);
-                //console.log("Data to stream ::", parsedJson);
-
-                liveStreamController.createCameraFolder(sendData, function (parsedJson, cameraFolder) {
-                    if (parsedJson.deviceType !== "Mobile") {
-                        liveStreamController.startLiveStreaming(parsedJson, cameraFolder);
-                        console.log("MQTT==================Start Streaming!!\n-----------------------------------\n");
-                    }
-                });
-                break;
-
-            /**
-             * Stop streaming
-             */
-            case "stopCamera":
-                var camIds = message.toString();
-                console.log("\n*Stop these cameras ::", JSON.stringify(camIds));
-                liveStreamController.stopCamera(camIds, function (error) {
-                    if (!error) {
-                        console.log("MQTT==================Stopped the Streaming Cameras\n-----------------------------------\n");
-                    }
-                });
-
-                break;
-            /**
-             * Stop/Start sending images to backend for a specific camera
-             */
-            case "toggleSendImageFlag":
-                var toggleObj = JSON.parse(message.toString());
-                console.log("Incoming : ", toggleObj.flag);
-                console.log("Incoming : ", toggleObj);
-                if (toggleObj.flag === 0 || toggleObj.flag === 1) {
-                    liveStreamController.toggleSendImageFlag(toggleObj.camId, toggleObj.flag);
-                } else {
-                    console.log("Error in ToggleSendImageFlag :: Invalid flag");
-                }
-                break;
-
-            /**
-             * video indexing
-             */
-            case "videoIndexingTopic":
-                var videoSourceData = message.toString();
-                var parsedJson = parseJson(videoSourceData);
-                videoIndexing(parsedJson);
-                console.log("Data sent for video indexing");
-                break;
-
-            default:
-                console.log("\n Default ::  Topic:: " + topic + " not handled!!");
+            });
         }
     });
 }

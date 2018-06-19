a44f8ed4702d8af60586958b0d772b1a639756c8
githubmobiliya <31727151+githubmobiliya@users.noreply.github.com>
2018-06-19 19:28:16 +0530

    revert back to interval change

--- controllers/liveStreamingController.js
+++ controllers/liveStreamingController.js
@@ -13,7 +13,6 @@ const { exec } = require('child_process');
 var sendImagesToggleMap = new Map();
 //to keep track of live cameras
 var liveCamIntervalArray = [];
-var liveCamMap = new Map();
 
 var findIPOfIPCamera = function (parsedJson, callback) {
     if(parsedJson.deviceType !=='IP'){
@@ -128,7 +127,7 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
     console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     console.log("Starting stream with data::", JSON.stringify(parsedJson));
 
-    var fps = 25;
+    var fps = 20;
     //fps:frames per second, interval: call to function in interval {vCap.get(5); vCap.get(CV_CAP_PROP_FPS)}
 
     var pushedInterval = false;
@@ -138,15 +137,21 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
     var camId = parsedJson.camId;
     var detectionType = parsedJson.feature;
     var jetsonFolderPath = parsedJson.jetsonCamFolderLocation;
-
+    var bboxes = parsedJson.boundingBox;
+    var imageConfig = {
+        frameWidth: parsedJson.frameWidth.width,
+        frameHeight: parsedJson.frameWidth.height,
+        ImageWidth: parsedJson.imageWidth,
+        ImageHeight: parsedJson.imageHeight
+    }
     var camName = parsedJson.deviceName;
     var userId = parsedJson.userId;
-
+    var cloudServiceUrl = parsedJson.cloudServiceUrl;
     var wayToCommunicate = parsedJson.wayToCommunicate;
     var expectedFPS = parsedJson.computeEngineFps;
     var deviceType = parsedJson.deviceType;
     var retryTime = 1000; //time interval after which openStream will try open the stream pipeline
-	parsedJson.cameraFolder = cameraFolder;
+
     //Webcam
     if (streamingUrl === "1") {
         streamingUrl = parseInt(streamingUrl);
@@ -161,7 +166,7 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
 
     var interval = fps;
     expectedFPS = parseInt(fps / expectedFPS);
-    parsedJson.expectedFPS = expectedFPS;
+
     //open the stream
     var vCap;
     openStream(streamingUrl, retryTime, function (cap) {
@@ -175,40 +180,72 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
             /**
             * To stream continuous frames with interval
             */
-           // var camInterval = setInterval(function () {
+            var camInterval = setInterval(function () {
 
                 if (pushedInterval == false) {
                     /**To maintain live camera array */
                     liveCamIntervalArray.push({
                         camId: camId,
-                        //intervalObj: camInterval,
-                        vCapObj: vCap,
-			stop:false
+                        intervalObj: camInterval,
+                        vCapObj: vCap
                     });
                     sendImagesToggleMap.set(camId, 0);
                     pushedInterval = true;
                 }
-
-		liveCamData = {
-                        vCapObj: vCap,
-			stop:false
-                    }
-		liveCamMap.set(camId, liveCamData);
-		console.log("Camera started =================",liveCamMap);
                 /**reading next frame */
                 if (vCap != null) {
-                    //vCap.read((err, frame) =>{
-                      	countFrame =0 ;
-			readFrame(vCap,parsedJson, countFrame,expectedFPS);
-                        //countframe = countframe + 1;
-			//});
-                   // });
-                }
+                    vCap.read(function (err, frame) {
+                        if (err) { console.log(err); }
 
+                        else {
+                            if (countframe % fps == 0) {
+                                //countframe reset
+                                countframe = 0;
+                                var timestamp = new Date().getTime();
+                                //composing imagename
+                                var imageName = camId + "_" + detectionType + "_" + timestamp + ".jpg";
+                                var imageFullPath = filePath + imageName;
+
+                                /**to write captured image of camera into local fs */
+                                frame.save(imageFullPath);
+                                frame.release();
+                                delete frame;
+
+                                //send images to Backend
+                                if (sendImagesToggleMap.get(camId) || parsedJson.sendImagesFlag) {
+                                    var outBase64 = imageProcessingController.base64_encode(imageFullPath);
+                                    imageTransfer.sendImageRest(imageName, config.sendLiveStreamUploadURL, outBase64);
+                                }
+                                //send to respective compute engine
+                                switch (wayToCommunicate) {
+                                    case 'rsync':
+                                        /**
+                                        * to sync newly added file with compute engine's FS
+                                        */
+                                        //imageTransfer.rsyncInterval(0, imageName, imageFullPath, camId, jetsonFolderPath);
+                                        break;
+
+                                    case 'restAPI':
+                                        /**
+                                        * to send images to cloud compute engine
+                                        */
+                                        imageTransfer.sendImageCloudComputeEngine(timestamp, imageFullPath, bboxes,
+                                            imageConfig, config.cloudServiceTargetUrl, cloudServiceUrl, camName, userId, camId); // cloudServiceUrl
+                                        break;
+
+                                    default:
+                                        console.log("Warning : Default Case executed ( specified way of communication not available:-  " + wayToCommunicate
+                                            + " not served yet)!");
+                                }
+                            }
+                        }
+                        countframe = countframe + 1;
+                    });
+                }
                 else {
                     console.log("**ERROR ::  In continuos streaming not able to stream cause Vcap is null !!")
                 }
-           // }, 2000 / interval);
+            }, 1000 / interval);
         }
         else {
             console.log("Unable to start the stream..!!");
@@ -216,94 +253,6 @@ var startLiveStreaming = function (parsedJson, cameraFolder) {
         }
     });
 }
-
-var readFrame = function(vCap, parsedJson, countframe ,expectedFPS){
-  camData = liveCamMap.get(parsedJson.camId);
-  stopOrNot = camData.stop;
- //console.log(expectedFPS);
-	
-  if(!stopOrNot){
-  vCap.read((err, frame) =>{
-	//callback(frame);
-        var timestamp = new Date().getTime();
-	if (err) { console.log(err); }
-
-        else {
-            if (countframe % expectedFPS === 0) {
-                //countframe reset
-                countframe = 0;
-		processImage(parsedJson, frame, vCap, expectedFPS, timestamp, function(vCap){
-                	countframe = countframe + 1;	//console.log(countFrame);
-			readFrame(vCap, parsedJson, countframe, expectedFPS);									
-		});
-            }else{
-                	countframe = countframe + 1;	
-			readFrame(vCap, parsedJson, countframe, expectedFPS);	
-	 }
-	}
- 	});
-   }else{
-		vCap.release();
-                delete vCap;
-	console.log("Stopped Camera",parsedJson.camId); 
-}
-}
-
-var processImage = function(parsedJson, frame, vCap, expectedFPS,timestamp, callback){
-var camId = parsedJson.camId;    var filePath = parsedJson.cameraFolder + "/";
-    var detectionType = parsedJson.feature;
-    var jetsonFolderPath = parsedJson.jetsonCamFolderLocation;    var wayToCommunicate = parsedJson.wayToCommunicate;
-    var camName = parsedJson.deviceName;
-    var userId = parsedJson.userId;
-	var bboxes = parsedJson.boundingBox;
-	var imageConfig = {
-	frameWidth: parsedJson.frameWidth.width,
-	frameHeight: parsedJson.frameWidth.height,
-	ImageWidth: parsedJson.imageWidth,
-	ImageHeight: parsedJson.imageHeight
-	}
-    	var cloudServiceUrl = parsedJson.cloudServiceUrl;
-
-
-        //composing imagename
-        var imageName = camId + "_" + detectionType + "_" + timestamp + ".jpg";
-        var imageFullPath = filePath + imageName;
-
-        /**to write captured image of camera into local fs */
-        frame.save(imageFullPath);
-        frame.release();
-        delete frame;
-
-        //send images to Backend
-        if (sendImagesToggleMap.get(camId) || parsedJson.sendImagesFlag) {
-            var outBase64 = imageProcessingController.base64_encode(imageFullPath);
-            imageTransfer.sendImageRest(imageName, config.sendLiveStreamUploadURL, outBase64);
-        }
-        //send to respective compute engine
-        switch (wayToCommunicate) {
-            case 'rsync':
-                /**
-                * to sync newly added file with compute engine's FS
-                */
-                //imageTransfer.rsyncInterval(0, imageName, imageFullPath, camId, jetsonFolderPath);
-                break;
-
-            case 'restAPI':
-                /**
-                * to send images to cloud compute engine
-                */
-		console.log(imageFullPath);
-                imageTransfer.sendImageCloudComputeEngine(timestamp, imageFullPath, bboxes,
-                    imageConfig, config.cloudServiceTargetUrl, cloudServiceUrl, camName, userId, camId); // cloudServiceUrl
-                break;
-
-            default:
-                console.log("Warning : Default Case executed ( specified way of communication not available:-  " + wayToCommunicate
-                    + " not served yet)!");
-        }
-	callback(vCap,expectedFPS);
-}
-
 
 /**
  * images toggling
@@ -328,24 +277,17 @@ var stopCamera = function (message, callback) {
     let tempArr = liveCamIntervalArray.slice();
 
     tempArr.forEach(function (cam, i) {
-	  camData = liveCamMap.get(cam.camId);
-	  if(camData){
-
-	  camData.stop=true;}
-        //if (camIds.includes(cam.camId)) {
+        if (camIds.includes(cam.camId)) {
             //to remove stopped live camera 
-
-            //clearInterval(cam.intervalObj);
-          //  if (cam.vCapObj != null) {
-            //    cam.vCapObj.release();
-              //  delete cam.vCapObj;
-            //}
-	    //cam.stop = true;
-            //console.log(" Stopped :: ", cam.camId);
-            //liveCamIntervalArray.splice(i, i + 1);
-        //}
+	    clearInterval(cam.intervalObj);
+            if (cam.vCapObj != null) {
+                //cam.vCapObj.release();
+                delete cam.vCapObj;
+            }
+            console.log(" Stopped :: ", cam.camId);
+            liveCamIntervalArray.splice(i, i + 1);
+        }
     });
-    callback(null);
 };
 
 module.exports.createCameraFolder = createCameraFolder;
